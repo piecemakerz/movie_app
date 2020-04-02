1c6ef58193ef683360e4e637e1d4242970a57e6d
piecemakerz <piecemakerz@naver.com>
2020-04-02 20:33:00 +0900

    Updates

---
+++ precache-manifest.b3d71155b07b708da2656f448b6c4bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c9857dbdc50156c94bc01f7aa97690",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "53ccdbedb23d0bd1972f",
+    "url": "/movie_app/static/css/main.044a078d.chunk.css"
+  },
+  {
+    "revision": "ae9741a940698ad698f4",
+    "url": "/movie_app/static/js/2.bb5f6ec6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.bb5f6ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ccdbedb23d0bd1972f",
+    "url": "/movie_app/static/js/main.862a6f80.chunk.js"
+  },
+  {
+    "revision": "1ba9e9bc64bc930888ed",
+    "url": "/movie_app/static/js/runtime-main.6ea79d3c.js"
+  }
+]);
